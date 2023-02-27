badaf81285486725e6c9d78d200fd6f1c13cbe7c
ahoplock <68440291+ahoplock@users.noreply.github.com>
2023-02-27 12:55:11 -0600

    Merging in the latest FMDB code from SVN (Log4j vulnerability fixes)

--- MyFMDB/FMDB-ejb/src/main/java/ca/bc/gov/moh/fmdb/logging/Log4JLoggerImpl.java
+++ MyFMDB/FMDB-ejb/src/main/java/ca/bc/gov/moh/fmdb/logging/Log4JLoggerImpl.java
@@ -12,96 +12,106 @@
 
 package ca.bc.gov.moh.fmdb.logging;
 
-import org.apache.log4j.Level;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 
 /**
- * 
+ *
  * Log4J implementation of the logger.
- * 
+ *
  * @author david.tulk
  */
-public class Log4JLoggerImpl implements Logger, java.io.Serializable {
-    private org.apache.log4j.Logger logger;
-    
-    public Log4JLoggerImpl(String name) {
-        this.logger = org.apache.log4j.Logger.getLogger(name);
-    }
-    
-    public Log4JLoggerImpl(Class name) {
-        this.logger = org.apache.log4j.Logger.getLogger(name);
-    }
-        
+public class Log4JLoggerImpl implements ca.bc.gov.moh.fmdb.logging.Logger, java.io.Serializable {
+
+    static final Logger logger = LogManager.getLogger(Log4JLoggerImpl.class.getName());
+
+    @Override
     public String getName() {
         return this.logger.getName();
     }
 
+    @Override
     public void debug(Object message) {
         this.log(Level.DEBUG, message, null);
     }
 
+    @Override
     public void debug(Object message, Throwable t) {
         this.log(Level.DEBUG, message, t);
     }
 
+    @Override
     public void error(Object message) {
         this.log(Level.ERROR, message, null);
     }
 
+    @Override
     public void error(Object message, Throwable t) {
-       this.log(Level.ERROR, message, t);
+        this.log(Level.ERROR, message, t);
     }
-    
+
+    @Override
     public void fatal(Object message) {
         this.log(Level.FATAL, message, null);
     }
-    
+
+    @Override
     public void fatal(Object message, Throwable t) {
         this.log(Level.FATAL, message, t);
     }
 
+    @Override
     public void info(Object message) {
-         this.log(Level.INFO, message, null);
+        this.log(Level.INFO, message, null);
     }
 
+    @Override
     public void info(Object message, Throwable t) {
-         this.logger.log(Level.INFO, message.toString(), t);
+        this.logger.log(Level.INFO, message.toString(), t);
     }
 
+    @Override
     public void warn(Object message) {
         this.log(Level.WARN, message, null);
     }
 
+    @Override
     public void warn(Object message, Throwable t) {
-         this.log(Level.WARN, message, t);
+        this.log(Level.WARN, message, t);
     }
-    
+
     private void log(Level level, Object message, Throwable t) {
-        if(t == null) {
-             this.logger.log(level, String.valueOf(message));
-        }
-        else {
-             this.logger.log(level, String.valueOf(message), t);
+        if (t == null) {
+            this.logger.log(level, String.valueOf(message));
+        } else {
+            this.logger.log(level, String.valueOf(message), t);
         }
     }
-    
+
+    @Override
     public boolean isDebugEnabled() {
         return this.logger.isDebugEnabled();
     }
 
+    @Override
     public boolean isErrorEnabled() {
-        return this.logger.isEnabledFor(Level.ERROR);
+        return this.logger.isEnabled(Level.ERROR);
     }
 
+    @Override
     public boolean isFatalEnabled() {
-        return this.logger.isEnabledFor(Level.FATAL);
+        return this.logger.isEnabled(Level.FATAL);
     }
 
+    @Override
     public boolean isInfoEnabled() {
         return this.logger.isInfoEnabled();
     }
 
+    @Override
     public boolean isWarnEnabled() {
-        return this.logger.isEnabledFor(Level.WARN);
+        return this.logger.isEnabled(Level.WARN);
     }
 
 }
