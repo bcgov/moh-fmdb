9affa2eed7eebdb067ad56c5e5a767f6dd4fad45
ahoplock <68440291+ahoplock@users.noreply.github.com>
2023-04-06 18:51:08 -0500

    Update CallbackServlet.java
    Revert temporary DNS changes

--- MyFMDB/FMDB-war/src/main/java/ca/bc/gov/hlth/security/CallbackServlet.java
+++ MyFMDB/FMDB-war/src/main/java/ca/bc/gov/hlth/security/CallbackServlet.java
@@ -21,6 +21,6 @@ public class CallbackServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.sendRedirect("https://fmdbd.hlth.gov.bc.ca/FMDB/pages/Search.xhtml");
+        response.sendRedirect(request.getContextPath() + "/pages/Search.xhtml");
     }
 }
