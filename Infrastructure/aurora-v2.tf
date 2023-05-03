a364816e4cbca689f92bc20803b883b2a3cd81bd
cat-panetta <123506599+cat-panetta@users.noreply.github.com>
2023-05-03 07:58:08 -0700

    Update to serverless

--- Infrastructure/aurora-v2.tf
+++ Infrastructure/aurora-v2.tf
@@ -39,6 +39,10 @@ resource "aws_db_subnet_group" "fmdb_subnet_group" {
 data "aws_rds_engine_version" "postgresql" {
   engine  = "aurora-postgresql"
   version = "13.9"
+  filter {
+    name = "engine-mode"
+    values = [ "serverless" ]
+  }
 }
 
 module "aurora_postgresql_v2" {
