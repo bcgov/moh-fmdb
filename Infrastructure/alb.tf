79fb5c1ef29c6bd8c4b7bc6430066510ec5e7baa
nathanielcx <51494277+nathanielcx@users.noreply.github.com>
2023-05-30 08:38:42 -0700

    Update alb.tf

--- Infrastructure/alb.tf
+++ Infrastructure/alb.tf
@@ -8,7 +8,7 @@ data "aws_alb" "main" {
 
 # Redirect all traffic from the ALB to the target group
 data "aws_alb_listener" "front_end" {
-  load_balancer_ = data.aws_alb.main.id
+  load_balancer_arn = data.aws_alb.main.id
   port              = 443
 }
 
